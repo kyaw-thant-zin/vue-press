00495379d9e2b6278552c292e8044d517409d26b
Kyaw Thant Zin <kyawthantzamt@gmail.com>
2023-01-21 12:34:12 +0630

    init wordpress starter theme and vue app

---
+++ footer.php
@@ -0,0 +1,13 @@
+<footer id="colophon" class="site-footer">
+    <div class="container">
+        <div class="site-info">
+            <p>Copyright &copy; <?php echo date( 'Y' ); ?> <?php bloginfo( 'name' ); ?></p>
+        </div><!-- .site-info -->
+    </div>
+</footer><!-- #colophon -->
+</div><!-- #content -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
