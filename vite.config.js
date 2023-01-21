6189e9cf23e017e9f601fe6c70abfb959d67b434
Kyaw Thant Zin <kyawthantzamt@gmail.com>
2023-01-21 18:31:51 +0630

    add Vite plugin browser sync

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import VitePluginBrowserSync from 'vite-plugin-browser-sync'
 
 // // https://vitejs.dev/config/
 // export default defineConfig({
@@ -17,6 +18,7 @@ export default defineConfig({
         },
       },
     }),
+    VitePluginBrowserSync()
   ],
   resolve: {
     alias: {
